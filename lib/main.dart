5f53a61c6a10715ed9359f249bb10159d03b0268
heidi <heidi@worksmate.co.kr>
2025-04-09 20:47:11 +0900

    feat. bottom navigation setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:esg_app/screens/home.dart';
+import 'package:esg_app/screens/login.dart';
 import 'package:flutter/material.dart' as material;
+import 'package:get/route_manager.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 void main() {
@@ -11,64 +14,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ShadcnApp(
-      title: '가칭',
-      theme: ThemeData(
-        colorScheme: ColorSchemes.lightGreen(),
-        radius: 0.5,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      headers: [AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Colors.green,
-        title: Text(widget.title),
-      )],
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            material.InkWell(
-              onTap: () {
-                setState(() {
-                  _counter ++;
-                });
-              },
-              child: Text(
-                '$_counter',
-                style: TextStyle(color: Colors.green, fontSize: 20.0),
-              ),
-            ),
-          ],
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: material.ThemeData(
+        colorScheme: material.ColorScheme.light(
+          primary: Color(0xFF34C759), // 메인 컬러 지정
         ),
       ),
+      routes: {'/': (context) => Home(), '/first': (context) => LoginScreen()},
     );
   }
 }
---
+++ lib/screens/mission.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MissionScreen extends StatefulWidget {
+  const MissionScreen({super.key});
+
+  @override
+  State<MissionScreen> createState() => _MissionScreenState();
+}
+
+class _MissionScreenState extends State<MissionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Mission')));
+  }
+}
