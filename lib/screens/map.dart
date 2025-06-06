781b0064b40e22944ab4ee99a4a4a6fc457e1a72
qnrjs42 <qnrjs42@naver.com>
2025-06-07 00:17:20 +0900

    feat. 내 위치 권한 요청

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -13,9 +13,12 @@ class _MapScreenState extends State<MapScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
-      FlutterNativeSplash.remove();
-    });
+
+    try {
+      requestGeolocationPermission();
+    } catch (error) {
+      print('error: $error');
+    }
   }
 
   @override
@@ -27,6 +30,47 @@ class _MapScreenState extends State<MapScreen> {
           print("네이버 맵 로딩됨!");
         },
       ),
+    );
+  }
+}
+
+Future<void> requestGeolocationPermission() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+  final GeolocatorPlatform geolocatorPlatform = GeolocatorPlatform.instance;
+
+  // Test if location services are enabled.
+  serviceEnabled = await geolocatorPlatform.isLocationServiceEnabled();
+  print('serviceEnabled: $serviceEnabled');
+
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await geolocatorPlatform.checkPermission();
+  print('permission: $permission');
+
+  if (permission == LocationPermission.denied) {
+    permission = await geolocatorPlatform.requestPermission();
+    print('permission request: $permission');
+
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.',
     );
   }
 }
