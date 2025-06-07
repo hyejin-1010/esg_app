58059fabcc531f2f0397c14af95a875700bdcd90
qnrjs42 <qnrjs42@naver.com>
2025-06-07 14:29:34 +0900

    feat. auth - sqlite 닉네임 중복 검사 추가

--- lib/controllers/auth.dart
+++ lib/controllers/auth.dart
@@ -1,18 +1,28 @@
+import 'package:esg_app/db/model_auth_dao.dart';
 import 'package:esg_app/models/auth.dart';
 import 'package:get/get.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:uuid/uuid.dart';
 
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
+String hashPassword(String password) {
+  final bytes = utf8.encode(password); // 문자열 → 바이트
+  final digest = sha256.convert(bytes); // SHA-256 해시 적용
+  return digest.toString(); // 해시 결과 문자열로 반환
+}
+
 class AuthController extends GetxController {
   late final User _user;
 
   Future<void> authJoin({
-    required String userId,
+    required String nickname,
     required String email,
     required String password,
   }) async {
     String uuid = const Uuid().v4();
-    User newUser = User(accessToken: uuid, userId: userId, email: email);
+    User newUser = User(accessToken: uuid, nickname: nickname, email: email);
 
     // 스토리지에 저장
     final storage = const FlutterSecureStorage();
@@ -45,6 +55,13 @@ class AuthController extends GetxController {
 
     // 2초 대기
     await Future.delayed(const Duration(seconds: 2));
+  }
+
+  Future<bool> authCheckDuplicateNickname({required String nickname}) async {
+    AuthDao authDao = AuthDao();
+    final isDuplicateNickname = await authDao.checkDuplicateNickname(nickname);
+
+    return isDuplicateNickname;
   }
 
   get user => _user;
